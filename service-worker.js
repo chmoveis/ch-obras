bdeebf7c8beed004102e3cca6f8cf2e2aec17b29
chmoveis <financeirochmoveis@gmail.com>
2026-05-29 10:04:36 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('ch-obras-cache-v82').then(function(cache) {
+    caches.open('ch-obras-cache-v83').then(function(cache) {
       const arquivosEssenciais = [
         'index.html',
         'manifest.json',
@@ -31,7 +31,7 @@ self.addEventListener('activate', function(event) {
       return Promise.all(
         nomesCaches
           .filter(function(nomeCache) {
-            return nomeCache.startsWith('ch-obras-cache-') && nomeCache !== 'ch-obras-cache-v82';
+            return nomeCache.startsWith('ch-obras-cache-') && nomeCache !== 'ch-obras-cache-v83';
           })
           .map(function(nomeCache) {
             return caches.delete(nomeCache);
